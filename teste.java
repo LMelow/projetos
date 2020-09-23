fc1cb8e5741e90cea13a3e924808db8d75397b1a
Rebonatto <leonardo@twoweb.com.br>
2020-09-23 14:34:40 -0300

    Melora a mensagem de oi

--- teste.java
+++ teste.java
@@ -1,7 +1,7 @@
 class main{
     public static void main(Strings args[]){
         // Comentários em java.
-        System.out.println("Hello World!");
+        System.out.println("Olá World!");
         System.out.println("Oi1")
     }
 }
