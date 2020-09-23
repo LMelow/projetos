52c74eeee588b43b9a829e937b3f6205233fa753
Rebonatto <leonardo@twoweb.com.br>
2020-09-23 14:29:21 -0300

    arruma identação

--- opera.java
+++ opera.java
@@ -1,5 +1,7 @@
 class Opera{
-  public static void main (String args[] ){
+  public static void main(Strings args[]){
+      //tetete coments
+      System.out.println("Hello World!");
+      System.out.println("Oi1")
   }
-}  
-  
+}
