b410d5e75cb25e61ba46c3002a7a3a25ce75303a
Rebonatto <leonardo@twoweb.com.br>
2020-09-23 14:24:01 -0300

    Refinamento de código

--- teste.java
+++ teste.java
@@ -1,6 +1,6 @@
 class main{
     public static void main(Strings args[]){
-        //tetete coments
+        // Comentários em java.
         System.out.println("Hello World!");
         System.out.println("Oi1")
     }
