87b6b60085c3e203c6fb759b2ef6d44ec9e69a51
Rebonatto <leonardo@twoweb.com.br>
2020-09-23 14:40:26 -0300

    merges

--- teste.java
+++ teste.java
@@ -1,5 +1,5 @@
 class main{
-    public static void main(Strings args[]){
+    public static void main(String args[]){
         // Comentários em java, adicionar coment pelo browser, teste.
         System.out.println("Olá Mundo!");
         System.out.println("Oi1")
